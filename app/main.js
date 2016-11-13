bbc54c61d98bf19d394757bf7488e1d19b3c8a25
Derek Smart <derek@grindaga.com>
2016-11-13 14:08:41 -0500

    remove old convention from main and index
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- app/main.js
+++ app/main.js
@@ -1,12 +1,9 @@
 const electron = require('electron')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
-const {ipcMain} = require('electron')
-const {dialog} = require('electron')
 
 const path = require('path')
 const url = require('url')
-const fs = require('fs');
 
 let mainWindow
 
@@ -39,21 +36,3 @@ app.on('activate', function () {
     createWindow()
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
-ipcMain.on('showOpenDialog', () => {
-  dialog.showOpenDialog((function (fileNames) {
-    if (fileNames === undefined) {
-      console.log('No file selected');
-    } else {
-      fs.readFile(fileNames[0], '', (function (err, data) {
-        if (err) {
-          this.alert(`An error ocurred reading the file : ${err.message}`);
-          return;
-        }
-        console.log(data);
-      }));
-    }
-  }));
-});
