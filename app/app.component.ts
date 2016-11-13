18f24a77622ffe39f309a39343ccfc90beb8b658
Derek Smart <derek@grindaga.com>
2016-11-13 14:08:16 -0500

    update to angular2 scructure
    Signed-off-by: Derek Smart <derek@grindaga.com>

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app',
+  template: `
+    <a href="#" id="fileselect" (click)="showOpenDialog();"></a>
+  `
+})
+export class AppComponent { }
