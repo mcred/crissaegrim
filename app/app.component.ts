1d6cbfd530fd1d62b77d229e71a8d6e8fc34f51c
Derek Smart <derek@grindaga.com>
2016-11-14 23:04:50 -0500

    move select file to html template
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,17 +1,11 @@
-/// <reference path="../node_modules/@types/electron/index.d.ts" />
-/// <reference path="../node_modules/@types/node/index.d.ts" />
-
 import { Component } from '@angular/core';
 import { ipcRenderer } from 'electron';
 
 @Component({
   selector: 'app',
-  template: `
-    <div id="selectFile">
-      <a href="#" id="fileselect" (click)="doSomething();"></a>
-    </div>
-  `
+  templateUrl: 'templates/selectFile.html'
 })
+
 export class AppComponent {
 	doSomething() {
 		var hexstring = ipcRenderer.send('showOpenDialog');
