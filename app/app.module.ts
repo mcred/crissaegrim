51851a386b426859285abe9e15d727f2cacbc01a
Derek Smart <derek@grindaga.com>
2016-11-16 22:00:59 -0500

    institute basic routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,22 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 import { AppComponent }   from './app.component';
 
+const routes: Routes = [
+  { path: '', component: AppComponent }
+]
+
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
   declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
+  bootstrap: [ AppComponent ]
 })
 
 export class AppModule { }
