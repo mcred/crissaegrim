748a12097f392f5395c7118fed3dcbe0f7705140
Derek Smart <derek@grindaga.com>
2016-11-22 00:03:19 -0500

    read file in character component
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- app/character.component.ts
+++ app/character.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ipcRenderer } from 'electron';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app',
@@ -7,4 +8,23 @@ import { ipcRenderer } from 'electron';
 })
 
 export class CharacterComponent {
+  hp: string;
+  hpmax: string;
+  file: string;
+  private sub: any;
+
+  constructor(
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+       this.file = params['file'];
+    });
+    console.log(parseInt("0x2474"));
+    console.log(this.file[9332]);
+    this.hp = this.file.charAt(parseInt("0x2474"));
+    this.hpmax = this.file.charAt(parseInt("0x248"));
+  }
+
 }
