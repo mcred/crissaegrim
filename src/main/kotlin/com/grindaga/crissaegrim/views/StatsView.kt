5c2c50acf309a84f7ce7ad2b1896c222476e3da3
Derek Smart <derek@grindaga.com>
2019-03-11 23:22:35 -0400

    fix relic layout to two column
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- src/main/kotlin/com/grindaga/crissaegrim/views/StatsView.kt
+++ src/main/kotlin/com/grindaga/crissaegrim/views/StatsView.kt
@@ -3,7 +3,6 @@ package com.grindaga.crissaegrim.views
 import com.grindaga.crissaegrim.controllers.EquipmentController
 import com.grindaga.crissaegrim.controllers.RelicsController
 import com.grindaga.crissaegrim.controllers.StatsController
-import com.grindaga.crissaegrim.model.Stat
 import javafx.scene.control.TextFormatter
 import tornadofx.*
 
@@ -13,10 +12,8 @@ class StatsView : View("My View") {
     private val relicsCtrl: RelicsController by inject()
     private val relics = relicsCtrl.relics
     private val equipmentCrl: EquipmentController by inject()
-
-    private val statsEditorView: StatsEditorView by inject()
-
     private val equipment = equipmentCrl.equipment
+
     private val tabWidth = 740.0/3
 
     private val eightBitFilter: (TextFormatter.Change) -> Boolean = { change ->
@@ -39,7 +36,6 @@ class StatsView : View("My View") {
             isClosable = false
             tabMinWidth = tabWidth
             tabMaxWidth = tabWidth
-            statsEditorView
 
         }
         tab("Equipment") {
@@ -58,20 +54,38 @@ class StatsView : View("My View") {
                     fieldset("Relics") {
                         hbox(80) {
                             vbox {
+                                var count: Int = 0
                                 for (relic in relics) {
-                                    field(relic.name) {
-                                        togglegroup {
-                                            togglebutton("Not Found", this, true, 0)
-                                            togglebutton("Off", this, false, 1)
-                                            togglebutton("On", this, false, 3)
-                                            bind(relic.valueProperty)
+                                    val row: Int = count % 2
+                                    if (row == 0) {
+                                        field(relic.name) {
+                                            togglegroup {
+                                                togglebutton("Not Found", this, true, 0)
+                                                togglebutton("Off", this, false, 1)
+                                                togglebutton("On", this, false, 3)
+                                                bind(relic.valueProperty)
+                                            }
                                         }
                                     }
+                                    count++
                                 }
                             }
-
                             vbox {
-
+                                var count: Int = 0
+                                for (relic in relics) {
+                                    val row: Int = count % 2
+                                    if (row == 1) {
+                                        field(relic.name) {
+                                            togglegroup {
+                                                togglebutton("Not Found", this, true, 0)
+                                                togglebutton("Off", this, false, 1)
+                                                togglebutton("On", this, false, 3)
+                                                bind(relic.valueProperty)
+                                            }
+                                        }
+                                    }
+                                    count++
+                                }
                             }
                         }
                     }
